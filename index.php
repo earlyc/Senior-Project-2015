9a0bca1cf45f7158767074e97f83467d232ccb1d
pbret281 <pb>
2015-10-06 14:52:09 -0600

    Very Rough layout
    Added a very rough potential layout of the control GUI

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="utf-8">
 	<title>AVATAR Control Interface</title>
-	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" type="text/css" href="css/stylesheet.css">
 	<!--[if IE]>
 		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
@@ -12,7 +12,29 @@
 
 <body>
 
-	<h1>Content goes here</h1>
+	<!-- Top center text -->
+	<h1 id="AVATARTopCenterText">AVATAR Control GUI</h1>
+	
+	<!-- AVATAR Movement Controls -->
+	<h1 id="AVATARMovementControlsLabel"> Movement Control</h1>
+	<!-- Move foward -->
+	<div id="moveFoward"> Move Foward </div>
+	<!-- Move left -->
+	<div id="moveLeft"> Move Left </div>
+	<!-- Move right -->
+	<div id="moveRight"> Move Right </div>
+	<!-- Move backwards -->
+	<div id="moveBackwards"> Move Backwards </div>
+	
+	<!-- AVATAR Video -->
+	<h1 id="AVATARVideoText"> AVATAR Video </div>
+	<div id="AVATARVideo"> </div>
+	
+	<!-- User Video -->
+	<h1 id="UserVideoText"> User Video </div>
+	<div id="UserVideo"> </div>
+	
+	
 
 </body>
 </html>
