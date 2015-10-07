0eb262d9a594d30e8d8bcf43d61f89e333df77af
earlyc <early.chris@gmail.com>
2015-10-06 19:56:59 -0500

    Event Handlers
    Added jQuery to the project using Google CDN.
    Also added ONE event handler for the move forward event.

--- index.php
+++ index.php
@@ -8,6 +8,20 @@
 	<!--[if IE]>
 		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
+	
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+	
+	<script type=text/javascript>
+		$(document).ready(function (){
+        	$("#moveForward").click(function(e) {
+            	e.preventDefault();
+				alert(this);
+				//this is where the event code goes for the moveForward event
+				//php script is executed and passed the proper parameter
+        	});
+    	});
+	</script>
+	
 </head>
 
 <body>
@@ -18,13 +32,13 @@
 	<!-- AVATAR Movement Controls -->
 	<h1 id="AVATARMovementControlsLabel"> Movement Control</h1>
 	<!-- Move foward -->
-	<div id="moveFoward"> Move Foward </div>
+	<div id="moveForward" class="moveButton"> Move Forward </div>
 	<!-- Move left -->
-	<div id="moveLeft"> Move Left </div>
+	<div id="moveLeft" class="moveButton"> Move Left </div>
 	<!-- Move right -->
-	<div id="moveRight"> Move Right </div>
+	<div id="moveRight" class="moveButton"> Move Right </div>
 	<!-- Move backwards -->
-	<div id="moveBackwards"> Move Backwards </div>
+	<div id="moveBackwards" class="moveButton"> Move Backwards </div>
 	
 	<!-- AVATAR Video -->
 	<h1 id="AVATARVideoText"> AVATAR Video </div>
