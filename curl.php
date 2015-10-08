62638eca0ae3d1f021a4b7a81acd23c61dd2b9a1
earlyc <early.chris@gmail.com>
2015-10-08 14:55:28 -0500

    added command parameter to curl.php

--- curl.php
+++ curl.php
@@ -1,6 +1,29 @@
 <?php
 
-$url = 'http://192.168.240.1/arduino/drive/forward/1';
+$command = _GET['command'];
+
+if($command == 'moveForward') {
+	$url = 'http://192.168.240.1/arduino/drive/forward/1';
+}
+else if($command == 'moveForward') {
+	$url = 'http://192.168.240.1/arduino/...'; //I don't know the REST commands here
+}
+else if($command == 'moveBackwards') {
+	$url = 'http://192.168.240.1/arduino/...';
+}
+else if($command == 'stop') {
+	$url = 'http://192.168.240.1/arduino/...';
+}
+else if($command == 'turnRight') {
+	$url = 'http://192.168.240.1/arduino/...';
+}
+else if($command == 'turnLeft') {
+	$url = 'http://192.168.240.1/arduino/...';
+}
+else if($command == 'turn180') {
+	$url = 'http://192.168.240.1/arduino/...';
+}
+
 $var = curl_init($url);
 curl_exec($var);
 
