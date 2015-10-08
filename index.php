cacc1dc39fce9dbf6e739dd53905f8786808ed99
earlyc <early.chris@gmail.com>
2015-10-08 14:43:25 -0500

    Clean-up & fixes

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php include("login.php"); ?>
-<?php include("logout.php"); ?>
-<?php include("curl.php"); ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -18,124 +16,38 @@
 		$(document).ready(function (){
         	$("#moveForward").click(function(e) {
             	e.preventDefault();
-				$.get("curl.php");//I put that
-				//alert(this);
-			
-        		
-				//this is where the event code goes for the moveForward event
-				//php script is executed and passed the proper parameter
-        		});
-    		});
-	</script>
-	
-	<!-- Forward Script -->
-	<script type=text/javascript>
-	function forwardFunction(){
-		
-		$.get("http://192.168.240.1/arduino/drive/forward/1");
-  
-	}
-	</script>
-	
-	<!-- Stop Script -->
-	<script type=text/javascript>
-	function stopFunction(){
-	
-		$.get("http://192.168.240.1/arduino/drive/stop/1");
-  
-	}
-	</script>
-	
-	<script type=text/javascript>
-	if (clicked_id =="1"{
-	
-		$.get("curl.php");
-	
-	<script type=text/javascript>
-		$(document).ready(function (){
-        	$("#moveForward").click(function(e) {
+				$.get("curl.php?command=moveForward");
+        	});
+    		$("#stop").click(function(e) {
             	e.preventDefault();
-				$.get("curl.php");//I put that
-				//alert(this);
-			
-        		
-				//this is where the event code goes for the moveForward event
-				//php script is executed and passed the proper parameter
-        		});
-    		});
-	</script>
-	
-	<script type=text/javascript>
-	function forwardFunction(){
-		
-		$.get("http://192.168.240.1/arduino/drive/forward/1");
-  
-	}
+				$.get("curl.php?command=stop");
+        	});
+    		$("#turnLeft").click(function(e) {
+            	e.preventDefault();
+				$.get("curl.php?command=turnLeft");
+        	});
+    		$("#turnRight").click(function(e) {
+            	e.preventDefault();
+				$.get("curl.php?command=turnRight");
+        	});
+    		$("#moveBackwards").click(function(e) {
+            	e.preventDefault();
+				$.get("curl.php?command=moveBackwards");
+        	});
+    		$("#turn180").click(function(e) {
+            	e.preventDefault();
+				$.get("curl.php?command=turn180");
+        	});
+    	});
 	</script>
 	
 </head>
 
 <body>
-<!--<button class="relayButton" type="button" id="1" 
-  onClick="buttonClick(this.id)">On</button>-->
-  
-<h1 class="title" id="AVATARTopCenterText">AVATAR Control GUI</h1>
 
 <!-- Logout Link calls the script in logout.php; redirects user to login page -->
 <a href="logout.php"> LOGOUT </a>
 
-<table style="width:100%">
-  <tr>
-    
-    <td><h1 id="AVATARMovementControlsLabel"> Movement Control</h1></td>		
-	<td><h1 id="AVATARMovementControlsLabel"> AVATAR Video </h1></td>
-  </tr>
-  <tr>
-    
-    
-  </tr>
- 
-</table>
-<table style="width:50%">
-  <tr>
-    <td></td>
-    <td><button class ="button" id ="moveForward" type="button" onclick="forwardFunction()">Forward</button></td>		
-    <td><button class ="button" id ="stop" onclick="stopFunction()">STOP</button></td>
-  </tr>
-  <tr>
-    <td><button class ="button"  id ="turnLeft" onclick="leftFunction()">Turn Left</button></td>
-    <td><button class ="button"  id ="moveBackwards" onclick="backwardsFunction()">Backwards</button></td>		
-    <td><button class ="button" id ="turnRight" onclick="rightunction()">Turn Right</button></td>
-  </tr>
-  <tr>
-    <td></td>
-    <td><button class ="button" id ="turn180" onclick="180Function()">Turn 180</button></td>		
-    <td></td>
-  </tr>
-</table>
-
-<h1 class="title" id="UserVideo">User Video</h1>
-
-	<script type=text/javascript>
-	function stopFunction(){
-	
-		$.get("http://192.168.240.1/arduino/drive/stop/1");
-  
-	}
-	</script>
-	
-	<script type=text/javascript>
-	if (clicked_id =="1"{
-	
-		$.get("curl.php");
-  
-	}
-	</script>
-</head>
-
-<body>
-<!--<button class="relayButton" type="button" id="1" 
-  onClick="buttonClick(this.id)">On</button>-->
   
 <h1 class="title" id="AVATARTopCenterText">AVATAR Control GUI</h1>
 <table style="width:100%">
