c1b67c83740b6a5341bb9701860b36ee753e6a51
pbret281 <pbretanus@gmail.com>
2015-11-13 11:34:33 -0600

    displays user video on AVATAR

---
+++ uservideopage.php
@@ -0,0 +1,54 @@
+<?php include("login.php"); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>AVATAR Control Interface</title>
+		<link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+		<!--[if IE]>
+			<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		
+		<script src="jquery-2.1.4.js" type=text/javascript></script>
+		<script src="siteScript.js" type=text/javascript></script>
+		<script>
+			window.onbeforeunload = function(e) {
+				return '';
+			};
+		</script>
+		<script src="simplewebrtc.js"></script>
+    	<script>
+		  var webrtc = new SimpleWebRTC({
+		  // the id/element dom element that will hold "our" video
+		  localVideoEl: 'localVideo',
+		  // the id/element dom element that will hold remote videos
+		  remoteVideosEl: 'AVATARVideoContainer',
+		  // immediately ask for camera access
+		  autoRequestMedia: true,
+		  url: 'http://192.168.1.122:8888/'
+		  });
+		  // we have to wait until it's ready
+		  webrtc.on('readyToCall', function () {
+		  // you can name it anything
+		  webrtc.joinRoom('AVATAR');
+		  });
+      	</script>
+		
+	</head>
+	
+	<body>
+		
+		<div id="wrap">
+			<div id="left">
+				<!-- <h1 class="box-title"> AVATAR Video </h1> -->
+				<div id="AVATARVideoContainer">
+				</div>
+				<h1 class="box-title">User Video</h1>
+				<div id="userVideoContainer">
+					<video id="localVideo" autoplay></video>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
